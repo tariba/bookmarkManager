3b34f0b57375a92cfd11689a0556264b929c699d
tariba <tariqxa@gmail.com>
2023-05-20 14:29:33 +0100

    server is listening

---
+++ bookmark_backend/app.js
@@ -0,0 +1,18 @@
+import express, { json } from 'express'
+// import router from './routeHandlers/index.js'
+//import { createTable } from './db/helperFunctions.js'
+//import { getAllData } from './modelFunctions/index.js'
+import cors from "cors";
+
+
+
+const app = express()
+const PORT = process.env.PORT
+app.use(cors("*"));
+app.use(express.json())
+app.use(cors("*"));
+// app.use('/bookmark', router)
+
+app.listen(PORT, ()=>{
+    console.log('I am listening')
+})
