d005404b056183a7044f970e34d5043b5cc814e8
tariba <tariqxa@gmail.com>
2023-05-21 19:36:36 +0100

    reployment with material ui

--- bookmark_manager/components/logout.js
+++ bookmark_manager/components/logout.js
@@ -1,5 +1,5 @@
 import Button from '@mui/material/Button';
-
+//logout functuon
 export default function Logout() {
     return (
       <Button color="inherit" a href="/api/auth/logout">Logout</Button>
